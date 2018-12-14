cc3d92429366752f56b9f8b9b5d86aa3bce55379
ABA Games <cs8k-cyu@asahi-net.or.jp>
2018-12-14 22:58:42 +0900

    Update game

--- src/samples/tadpole/index.ts
+++ src/samples/tadpole/index.ts
@@ -1,11 +1,9 @@
-import { spawn, addUpdater, reset, pool, Updater } from "../..";
-import { Actor, Rect } from "../util/canvas/actor";
-import { init, endGame, random, ticks, difficulty } from "../util/game";
+import { spawn, addUpdater, reset, Updater, pool } from "../..";
+import { Actor } from "../util/canvas/actor";
+import { init, endGame, random, ticks } from "../util/game";
 import * as screen from "../util/canvas/screen";
 import * as text from "../util/canvas/text";
 import * as pointer from "../util/pointer";
-import Vector from "../util/vector";
-import * as math from "../util/math";
 import * as sound from "../util/sound";
 
 let score = 0;
@@ -16,13 +14,17 @@ init({
   game: () => {
     reset();
     score = 0;
-    math.range(5).map(_ => spawn(player));
-    math.range(10).map(_ => spawn(enemy));
+    spawn(player, 50, 50);
+    spawn(bonus);
     addUpdater(() => {
       text.draw(`${score}`, 1, 1, { align: "left" });
       if (introTicks > 0) {
         text.draw("[CLICK][TAP]\nMOVE FORWARD", 5, 10, { align: "left" });
         introTicks--;
+      }
+      const plc = pool.get(player).length;
+      if (plc <= 0) {
+        endGame();
       }
     });
   },
@@ -44,40 +46,86 @@ init({
   },
   init: () => {
     document.title = "TADPOLE";
-    //sound.loadInstrument("kalimba");
   },
   screen: screen,
   actorClass: Actor,
-  onPointerUp: sound.resumeAudioContext,
-  isDebugMode: true
+  onPointerUp: sound.resumeAudioContext
+  //isDebugMode: true
 });
 
-function player(a: Actor) {
-  tadpole(a, "black");
+function player(a: Actor, x: number, y: number) {
+  tadpole(a, "black", 0.04, x, y);
+  a.addUpdater(() => {
+    a.getCollidings(bonus).forEach(c => {
+      c.remove();
+      const plc = a.pool.get(player).length;
+      score += plc;
+      for (let i = 0; i < Math.min(plc, 3); i++) {
+        spawn(bonus);
+      }
+      spawn(player, a.pos.x, a.pos.y);
+      spawn(enemy);
+    });
+  });
 }
 
 function enemy(a: Actor) {
-  tadpole(a, "coral");
+  tadpole(a, "coral", 0.02);
+  a.addUpdater(() => {
+    a.getCollidings(player).forEach(c => {
+      c.remove();
+    });
+  });
 }
 
-function tadpole(a: Actor, color: string) {
+function tadpole(a: Actor, color: string, angleVel: number, x = -1, y = -1) {
   a.setRect(5, 5, { color });
   a.addRectWithSquares(10, 3, { offset: { x: -4 }, color });
-  a.pos.set(random.get(20, 80), random.get(20, 80));
-  a.angle = random.get(Math.PI * 2);
-  let forwardTicks = 0;
-  a.addUpdater(() => {
-    a.angle += 0.02;
-    if (forwardTicks <= 0 && pointer.isJustPressed) {
-      forwardTicks = 30;
-      a.speed += 2;
-    }
-    if (forwardTicks > 0) {
-      a.speed *= 0.9;
-      forwardTicks--;
-      if (forwardTicks <= 0) {
-        a.speed = 0;
+  if (x >= 0) {
+    a.pos.set(x, y);
+  } else {
+    let isValid = false;
+    for (let i = 0; i < 9; i++) {
+      a.pos.set(random.get(99), random.get(99));
+      const p = a.getNearest(player);
+      if (p === false || p.pos.distanceTo(a.pos) > 30) {
+        isValid = true;
+        break;
       }
     }
+    if (!isValid) {
+      a.remove();
+      return;
+    }
+  }
+  a.angle = random.get(Math.PI * 2);
+  a.addUpdater(() => {
+    a.angle += angleVel;
+    a.speed += (0.1 - a.speed) * 0.07;
+    if (pointer.isJustPressed) {
+      a.speed = 2;
+    }
+    a.pos.wrap(0, 99, 0, 99);
+  });
+}
+
+function bonus(a: Actor) {
+  a.addRectWithSquares(10, 5, { color: "GreenYellow" });
+  a.addRectWithSquares(5, 10, { color: "GreenYellow" });
+  let isValid = false;
+  for (let i = 0; i < 9; i++) {
+    a.pos.set(random.get(99), random.get(99));
+    const p = a.getNearest(player);
+    if (p === false || p.pos.distanceTo(a.pos) > 30) {
+      isValid = true;
+      break;
+    }
+  }
+  if (!isValid) {
+    a.remove();
+    return;
+  }
+  a.addUpdater(() => {
+    a.angle += 0.1;
   });
 }
